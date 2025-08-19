af26dfa3f69ca63e340897dd936b7ff0c8ce0db6
Caden Helbling <63411686+Caden-Helbling@users.noreply.github.com>
2025-08-19 10:51:51 -0500

    Update build_and_push.sh
    add in host name generation

--- build_and_push.sh
+++ build_and_push.sh
@@ -6,7 +6,17 @@ GIT_COMMIT=$(git rev-parse --short HEAD)
 export IMAGE_TAG="${BRANCH_NAME}-${GIT_COMMIT}"
 export ECR_IMAGE_NAME="prediction:${IMAGE_TAG}"
 
+# Set ingress host based on branch (can still be overridden via environment variable)
+if [ -z "$INGRESS_HOST" ]; then
+    if [ "$BRANCH_NAME" = "main" ] || [ "$BRANCH_NAME" = "master" ]; then
+        export INGRESS_HOST="kind.neo.nsstc.uah.edu"
+    else
+        export INGRESS_HOST="${BRANCH_NAME}.neo.nsstc.uah.edu"
+    fi
+fi
+
 echo "Building and pushing image: $ECR_URL/$ECR_IMAGE_NAME"
+echo "Using ingress host: $INGRESS_HOST"
 
 # Build and push Docker image
 docker buildx build --platform linux/amd64 -t $ECR_URL/$ECR_IMAGE_NAME .
@@ -14,8 +24,9 @@ ECR_PASSWORD=$(aws ecr get-login-password --region us-west-2)
 echo $ECR_PASSWORD | docker login --username AWS --password-stdin $ECR_URL
 docker push $ECR_URL/$ECR_IMAGE_NAME
 
-# Generate deployment.yaml from template using envsubst
+# Generate deployment.yaml and ingress.yaml from templates using envsubst
 envsubst < k8s-manifests/deployment.template.yaml > k8s-manifests/deployment.yaml
+envsubst < k8s-manifests/ingress.template.yaml > k8s-manifests/ingress.yaml
 
 # Apply Kubernetes manifests
 kubectl apply -f k8s-manifests/
