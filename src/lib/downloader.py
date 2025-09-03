017cebbdc54dc685d83cc6c650724dcdd31716f8
xhagrg <grg.iksha@hotmail.com>
2025-09-03 11:09:07 -0500

    Add missing /.

--- src/lib/downloader.py
+++ src/lib/downloader.py
@@ -132,7 +132,7 @@ class Downloader:
             output_name: output file name
         """
 
-        output_name = f"{DOWNLOAD_FOLDER}{self.generate_digest(self.date, self.bbox)}.tif"
+        output_name = f"{DOWNLOAD_FOLDER.rstrip('/')}/{self.generate_digest(self.date, self.bbox)}.tif"
         if os.path.exists(output_name):
             print(f"File {output_name} already exists. Skipping merge.")
             return output_name
