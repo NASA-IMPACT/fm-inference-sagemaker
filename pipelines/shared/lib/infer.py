128f692078467240a65df09a0023aa6259137230
xhagrg <grg.iksha@hotmail.com>
2025-09-04 17:04:53 -0500

    Add proper check for length.

--- pipelines/shared/lib/infer.py
+++ pipelines/shared/lib/infer.py
@@ -16,7 +16,7 @@ class Infer:
         # Use proper mean and std from consts if not in config.
         self.means = np.asarray(self.config['data']['init_args'].get('means', MEANS))
         self.stds = np.asarray(self.config['data']['init_args'].get('stds', STDS))
-        if self.means and self.stds:
+        if len(self.means) > 0 and len(self.stds) > 0:
             self.means = self.means.view(-1, 1, 1)
             self.stds = self.stds.view(-1, 1, 1)
 
@@ -37,7 +37,7 @@ class Infer:
                 image = raster_file.read()[:6]  # Read first 6 bands
                 image = np.where(image == NO_DATA, NO_DATA_FLOAT, image)
                 image = torch.from_numpy(image)
-                if self.means and self.stds:
+                if len(self.means) > 0 and len(self.stds) > 0:
                     image = (image - self.means) / self.stds
                 images_array.append(image)
                 profiles.append(raster_file.profile)
