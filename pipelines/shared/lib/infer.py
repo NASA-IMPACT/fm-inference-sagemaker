b66b72389bc0cb4281890639977ed85e62a0fd0e
xhagrg <grg.iksha@hotmail.com>
2025-09-05 14:57:48 -0500

    Remove unwanted line.

--- pipelines/shared/lib/infer.py
+++ pipelines/shared/lib/infer.py
@@ -92,7 +92,6 @@ class Infer:
                     #         mode="nearest"
                     #     )
                     print("Shape of predicted mask:", predicted_mask.shape)
-                    print("max and min of predicted mask:", torch.max(predicted_mask), torch.min(predicted_mask))
                     print("max and min of predicted mask:", predicted_mask.max(), predicted_mask.min())
                 predicted_masks.append(predicted_mask)
             return predicted_masks, profiles
