38b3e32554e0e02404515c86b5dc3564017e4f3b
xhagrg <grg.iksha@hotmail.com>
2025-09-05 00:59:08 -0500

    Use proper width and height.

--- pipelines/floods/lib/flood_infer.py
+++ pipelines/floods/lib/flood_infer.py
@@ -10,7 +10,7 @@ class FloodInfer(Infer):
         dem_downloader = DEMDownloader(bbox, date)
         dem_files = dem_downloader.download_dem_tiles()
         with open(image) as src:
-            width, height = src.width, src.height
+            width, height = src.profile['width'], src.profile['height']
         dem_file = dem_downloader.merge_and_clip_dems(dem_files, width, height)
         final_prediction = dem_downloader.apply_all_postprocessing(prediction, image, dem_file)
         return final_prediction
