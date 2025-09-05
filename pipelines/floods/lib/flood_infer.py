230a23f4b3a7ca499f2648d308f44d5a0c25379c
xhagrg <grg.iksha@hotmail.com>
2025-09-04 23:23:57 -0500

    Remove named parameters.

--- pipelines/floods/lib/flood_infer.py
+++ pipelines/floods/lib/flood_infer.py
@@ -7,7 +7,7 @@ class FloodInfer(Infer):
         super().__init__(config, checkpoint)
 
     def postprocess(self, bbox, date, predictions, images):
-        dem_downloader = DEMDownloader(bbox=bbox, date=date)
+        dem_downloader = DEMDownloader(bbox, date)
         dem_files = dem_downloader.download_dem_tiles()
         dem_file = dem_downloader.merge_and_clip_dems(dem_files)
         final_predictions = dem_file.apply_all_postprocessing(predictions, images, dem_file)
