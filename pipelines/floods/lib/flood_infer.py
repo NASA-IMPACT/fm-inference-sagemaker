0feab441cf1c4c709cd606a589baf4d4e97d99b1
xhagrg <grg.iksha@hotmail.com>
2025-09-04 23:29:32 -0500

    Use proper variables.

--- pipelines/floods/lib/flood_infer.py
+++ pipelines/floods/lib/flood_infer.py
@@ -6,9 +6,9 @@ class FloodInfer(Infer):
     def __init__(self, config, checkpoint):
         super().__init__(config, checkpoint)
 
-    def postprocess(self, bbox, date, predictions, images):
+    def postprocess(self, bbox, date, prediction, image):
         dem_downloader = DEMDownloader(bbox, date)
         dem_files = dem_downloader.download_dem_tiles()
         dem_file = dem_downloader.merge_and_clip_dems(dem_files)
-        final_predictions = dem_file.apply_all_postprocessing(predictions, images, dem_file)
-        return final_predictions
+        final_prediction = dem_downloader.apply_all_postprocessing(prediction, image, dem_file)
+        return final_prediction
