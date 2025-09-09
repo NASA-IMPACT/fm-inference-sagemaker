f96b75d6edcd230d7670fbd7e12a3f3b545a7414
xhagrg <grg.iksha@hotmail.com>
2025-09-09 16:21:09 -0500

    Use build args for base images.

--- build_and_push.sh
+++ build_and_push.sh
@@ -20,7 +20,7 @@ ECR_PASSWORD=$(aws ecr get-login-password --region us-west-2)
 echo $ECR_PASSWORD | docker login --username AWS --password-stdin $ECR_URL
 
 # Build and push base image first
-BASE_IMAGE_NAME="inference:temp"
+BASE_IMAGE_NAME="inference_pipelines:temp"
 echo "Building temporary image to get digest: $BASE_IMAGE_NAME"
 docker buildx build --platform linux/amd64 -t $BASE_IMAGE_NAME . -f Dockerfile.base
 BASE_DIGEST=$(docker inspect --format='{{.Id}}' $BASE_IMAGE_NAME | cut -d: -f2 | cut -c1-12)
@@ -31,11 +31,11 @@ docker push $ECR_URL/$ECR_BASE_IMAGE_NAME
 # Build floods and burn scars images
 TEMP_FLOOD_IMAGE_NAME="floods:temp"
 echo "Building temporary image to get digest: $TEMP_FLOOD_IMAGE_NAME"
-docker buildx build --platform linux/amd64 -t $TEMP_FLOOD_IMAGE_NAME . -f floods/Dockerfile
+docker buildx build --platform linux/amd64 -t $TEMP_FLOOD_IMAGE_NAME . -f floods/Dockerfile --build-arg BASE_IMAGE=$ECR_URL/$ECR_BASE_IMAGE_NAME
 
 TEMP_BURN_IMAGE_NAME="burn_scars:temp"
 echo "Building temporary image to get digest: $TEMP_BURN_IMAGE_NAME"
-docker buildx build --platform linux/amd64 -t $TEMP_BURN_IMAGE_NAME . -f burn_scars/Dockerfile
+docker buildx build --platform linux/amd64 -t $TEMP_BURN_IMAGE_NAME . -f burn_scars/Dockerfile --build-arg BASE_IMAGE=$ECR_URL/$ECR_BASE_IMAGE_NAME
 
 # Get the image digest (content-based hash) - extract only the hash portion
 IMAGE_DIGEST=$(docker inspect --format='{{.Id}}' $TEMP_IMAGE_NAME | cut -d: -f2 | cut -c1-12)
