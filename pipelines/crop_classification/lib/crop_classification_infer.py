b65d0a3ce7a48c147f86f535447b2704edf1bd61
xhagrg <grg.iksha@hotmail.com>
2025-09-11 00:18:10 -0500

    Handle rearrange properly.

--- pipelines/crop_classification/lib/crop_classification_infer.py
+++ pipelines/crop_classification/lib/crop_classification_infer.py
@@ -98,7 +98,7 @@ class CropClassificationInfer(Infer):
         # forward the model
         with torch.no_grad():
             images, profiles, coords, temporal = self.preprocess(images, date)
-            images = rearrange(images, 'b (c t) h w -> b c t h w', channels=6)
+            images = rearrange(images, 'b (c t) h w -> b c t h w', c=6, t=3)
             result = self.model(
                 images.to('cuda' if torch.cuda.is_available() else 'cpu')
                 # torch.tensor(temporal).to('cuda' if torch.cuda.is_available() else 'cpu'),
