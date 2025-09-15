f55955d1f027ff4cf3aec6810100dbd96c2954cf
amarouane-ABDELHAK <am0089@uah.edu>
2025-09-15 13:18:38 -0500

    Finetune model

--- src/api/v1/finetuned_models.py
+++ src/api/v1/finetuned_models.py
@@ -28,7 +28,7 @@ def get_models(db: Session = Depends(get_db)):
         return {"error": str(e)}
 
 
-@router.post("/", response_model=FinetunedModelRead, status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=FinetunedModel, status_code=status.HTTP_201_CREATED)
 def create_model(model: FinetunedModelUpdate, db: Session = Depends(get_db)):
     """Create a new finetuned model."""
     try:
