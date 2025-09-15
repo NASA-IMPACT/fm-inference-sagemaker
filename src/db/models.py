70d3d94d4edfae5219b9f23bc4218af837ebf4dc
xhagrg <grg.iksha@hotmail.com>
2025-09-15 15:23:44 -0500

    Add missing import.

--- src/db/models.py
+++ src/db/models.py
@@ -1,5 +1,5 @@
 import uuid
-from datetime import datetime
+from datetime import datetime, timezone
 from sqlalchemy import (
     Column, String, DateTime, Enum, ForeignKey, Boolean, Table, JSON, CheckConstraint
 )
