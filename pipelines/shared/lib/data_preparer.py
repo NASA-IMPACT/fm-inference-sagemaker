b59e326a65336a7dd4b9ecaf60937a5a9014b55d
xhagrg <grg.iksha@hotmail.com>
2025-09-25 14:48:16 -0500

    Use 512 X 512 for inference.

--- pipelines/shared/lib/data_preparer.py
+++ pipelines/shared/lib/data_preparer.py
@@ -7,7 +7,7 @@ from lib.consts import DOWNLOAD_FOLDER
 from rasterio.io import MemoryFile
 from rasterio.windows import from_bounds, Window
 
-SHAPE = (224, 224)
+SHAPE = (512, 512)
 
 QA_INDICES = {
     'cloud': 1,
