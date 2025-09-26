90db2e676e97f7525df6d0d63f76dffe124265b5
amarouane-ABDELHAK <am0089@uah.edu>
2025-09-26 16:07:10 -0500

    Add auth locks to fastapi endpoints

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -1,9 +1,9 @@
 from .inferences import create_inference_router
-from .finetuned_models import router as models_router
+from .finetuned_models import create_models_router
 from .preloaded_events import router as preloaded_events_router
 
 __all__ = [
     "create_inference_router",
-    "models_router",
+    "create_models_router",
     "preloaded_events_router"
 ]
--- src/api/v1/finetuned_models.py
+++ src/api/v1/finetuned_models.py
@@ -1,5 +1,5 @@
-from typing import Dict, List
-from fastapi import APIRouter, Depends, Query, status
+from typing import Dict, List, Callable, Any
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from ...db.database import get_db
@@ -9,94 +9,109 @@ from ...models.finetuned_model import FinetunedModelRead, FinetunedModelUpdate
 from ...models.inference import InferenceRead
 from ...models.preloaded_event import PreloadedEventRead
 
-router = APIRouter(prefix="/v1/models", tags=["models"])
+def create_models_router(auth_dependency: Callable) -> APIRouter:
+    router = APIRouter(prefix="/v1/models", tags=["models"])
 
-@router.get("", response_model=List[FinetunedModelRead], status_code=status.HTTP_200_OK)
-def get_models(db: Session = Depends(get_db)):
-    """Get all finetuned models."""
-    try:
-        # Total jobs by status
-        models = db.query(
-            FinetunedModel
-        ).all()
-        print('----------------')
-        print(len(models))
-        return models
-    except Exception as e:
-        return {"error": str(e)}
-
-
-@router.post("", status_code=status.HTTP_201_CREATED)
-def create_model(model: FinetunedModelUpdate, db: Session = Depends(get_db)):
-    """Create a new finetuned model."""
-    try:
-        db_model = FinetunedModel(
-            name=model.name,
-            source_type=model.source_type,
-            source_details=model.source_details,
-            data_config=model.data_config
-        )
-        db.add(db_model)
-        db.commit()
-        db.refresh(db_model)
-        return {"message": f"Model {model.name} was created successfully!"}
-    except Exception as e:
-        return {"error": str(e)}
+    @router.get("", response_model=List[FinetunedModelRead], status_code=status.HTTP_200_OK)
+    def get_models(claims: Dict[str, Any] = Depends(auth_dependency), db: Session = Depends(get_db)):
+        """Get all finetuned models."""
+        if not claims:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="No valid token provided" )
+        try:
+            # Total jobs by status
+            models = db.query(
+                FinetunedModel
+            ).all()
+            print('----------------')
+            print(len(models))
+            return models
+        except Exception as e:
+            return {"error": str(e)}
 
 
-
-
-
-@router.get("/{model_id}", response_model=FinetunedModelRead, status_code=status.HTTP_200_OK)
-def get_model(model_id: str, db: Session = Depends(get_db)):
-    """Get a specific model by ID."""
-    try:
-        model = db.query(FinetunedModel).filter(FinetunedModel.id == model_id).first()
-        if not model:
-            return {"error": "Model not found"}
-        return model
-    except Exception as e:
-        return {"error": str(e)}
-
-@router.get("/{model_id}/inferences", response_model=List[InferenceRead], status_code=status.HTTP_200_OK)
-def get_model_inferences(model_id: str, db: Session = Depends(get_db)):
-    """Get inferences associated with a specific model."""
-    try:
-        inferences = db.query(Inference).join(
-            Inference.finetuned_models
-        ).filter(
-            FinetunedModel.id == model_id
-        ).all()
-        return inferences
-    except Exception as e:
-        return {"error": str(e)}
-
-@router.get("/{model_id}/preloaded_events", response_model=List[PreloadedEventRead], status_code=status.HTTP_200_OK)
-def get_model_preloaded_events(model_id: str, db: Session = Depends(get_db)):
-    """Get preloaded events associated with a specific model."""
-    try:
-        events = db.query(PreloadedEvent).join(
-            PreloadedEvent.inference
-        ).join(
-            Inference.finetuned_models
-        ).filter(
-            FinetunedModel.id == model_id
-        ).all()
-        return events
-    except Exception as e:
-        return {"error": str(e)}
+    @router.post("", status_code=status.HTTP_201_CREATED)
+    def create_model(model: FinetunedModelUpdate,claims: Dict[str, Any] = Depends(auth_dependency), db: Session = Depends(get_db)):
+        """Create a new finetuned model."""
+        user_groups = claims.get("groups") or claims.get("cognito:groups", [])
+        if model.name not in user_groups + ["admin"]:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"You do not have permission to create {model.name} model")
+        try:
+            db_model = FinetunedModel(
+                name=model.name,
+                source_type=model.source_type,
+                source_details=model.source_details,
+                data_config=model.data_config
+            )
+            db.add(db_model)
+            db.commit()
+            db.refresh(db_model)
+            return {"message": f"Model {model.name} was created successfully!"}
+        except Exception as e:
+            return {"error": str(e)}
 
 
 
-@router.delete("/{model_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_model(model_id: str, db: Session = Depends(get_db)):
-    """Delete a finetuned model by ID."""
-    try:
-        model = db.query(FinetunedModel).filter(FinetunedModel.id == model_id).first()
-        if not model:
-            return {"error": "Model not found"}
-        db.delete(model)
-        db.commit()
-        return {"message": "Model deleted successfully"}
-    except Exception as e:
-        return {"error": str(e)}
+
+
+    @router.get("/{model_id}", response_model=FinetunedModelRead, status_code=status.HTTP_200_OK)
+    def get_model(model_id: str,claims: Dict[str, Any] = Depends(auth_dependency), db: Session = Depends(get_db)):
+        """Get a specific model by ID."""
+        if not claims:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="No valid token provided" )
+        try:
+            model = db.query(FinetunedModel).filter(FinetunedModel.id == model_id).first()
+            if not model:
+                return {"error": "Model not found"}
+            return model
+        except Exception as e:
+            return {"error": str(e)}
+
+    @router.get("/{model_id}/inferences", response_model=List[InferenceRead], status_code=status.HTTP_200_OK)
+    def get_model_inferences(model_id: str, claims: Dict[str, Any] = Depends(auth_dependency), db: Session = Depends(get_db)):
+        """Get inferences associated with a specific model."""
+        if not claims:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="No valid token provided" )
+        try:
+            inferences = db.query(Inference).join(
+                Inference.finetuned_models
+            ).filter(
+                FinetunedModel.id == model_id
+            ).all()
+            return inferences
+        except Exception as e:
+            return {"error": str(e)}
+
+    @router.get("/{model_id}/preloaded_events", response_model=List[PreloadedEventRead], status_code=status.HTTP_200_OK)
+    def get_model_preloaded_events(model_id: str, db: Session = Depends(get_db)):
+        """Get preloaded events associated with a specific model."""
+        try:
+            events = db.query(PreloadedEvent).join(
+                PreloadedEvent.inference
+            ).join(
+                Inference.finetuned_models
+            ).filter(
+                FinetunedModel.id == model_id
+            ).all()
+            return events
+        except Exception as e:
+            return {"error": str(e)}
+
+
+
+    @router.delete("/{model_id}", status_code=status.HTTP_204_NO_CONTENT)
+    def delete_model(model_id: str,claims: Dict[str, Any] = Depends(auth_dependency), db: Session = Depends(get_db)):
+        """Delete a finetuned model by ID."""
+        try:
+            model = db.query(FinetunedModel).filter(FinetunedModel.id == model_id).first()
+            user_groups = claims.get("groups") or claims.get("cognito:groups", [])
+            if not model:
+                return {"error": "Model not found"}
+            # Only admins can delete models
+            if user_groups != ["admin"]:
+                raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"You do not have permission to delete {model.name} model")
+            db.delete(model)
+            db.commit()
+            return {"message": "Model deleted successfully"}
+        except Exception as e:
+            return {"error": str(e)}
+    return router
