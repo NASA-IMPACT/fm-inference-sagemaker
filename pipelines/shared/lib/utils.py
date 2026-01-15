64c3f13bbd02f3ff2791f2db390585f6959af8de
Deepak <shahkrdeepak@gmail.com>
2026-01-15 15:44:21 -0600

    move upload_cog_to_s3 to utils

--- pipelines/shared/lib/utils.py
+++ pipelines/shared/lib/utils.py
@@ -3,6 +3,10 @@ import os
 
 from os import path
 from glob import glob
+from rasterio.io import MemoryFile
+from rio_cogeo.cogeo import cog_translate
+from rio_cogeo.profiles import cog_profiles
+
 from lib.consts import BUCKET_NAME, MODEL_PATH
 
 
@@ -38,3 +42,40 @@ def save_model_artifacts(s3_connection, model_artifacts_path):
             model_name = MODEL_PATH.format(model_name=model_name)
             print(f"Uploading model to s3: s3://{BUCKET_NAME}/{model_name}")
             s3_connection.meta.client.upload_file(model_file, BUCKET_NAME, model_name)
+
+
+def upload_cog_to_s3(filename: str, prefix: str = "predictions") -> str:
+    """
+    Convert a GeoTIFF to COG in-memory and upload to S3.
+
+    Returns the s3:// URL. 
+    """
+    if not BUCKET_NAME:
+        # Fail gracefully in local setups without S3
+        return filename
+
+    output_profile = cog_profiles.get("deflate")
+    output_profile.update(dict(BIGTIFF="IF_SAFER"))
+
+    config = dict(
+        GDAL_NUM_THREADS="ALL_CPUS",
+        GDAL_TIFF_INTERNAL_MASK=True,
+        GDAL_TIFF_OVR_BLOCKSIZE="512",
+    )
+
+    basename = os.path.basename(filename)
+    s3_prefix = f"{prefix}/{basename}"
+
+    with MemoryFile() as memory_file:
+        cog_translate(
+            filename,
+            memory_file.name,
+            output_profile,
+            config=config,
+            quiet=True,
+            in_memory=True,
+        )
+        connection = boto3.client("s3")
+        connection.upload_fileobj(memory_file, BUCKET_NAME, s3_prefix)
+
+    return f"s3://{BUCKET_NAME}/{s3_prefix}"
