b8dcb4ca4d85631f3ec19cce1b6612b1cda79fdd
xhagrg <grg.iksha@hotmail.com>
2026-05-19 20:47:39 -0500

    Fix issues with num_classes.

--- code/lib/infer.py
+++ code/lib/infer.py
@@ -121,7 +121,8 @@ class Infer:
             results = result.output.detach().cpu()
             for index, mask in enumerate(results):
                 output = mask.cpu()  # [n_segmentation_class, 224, 224]
-                if self.config['data']['init_args']['num_classes'] == 1:
+                num_classes = self.config["model"]["init_args"]["model_args"]["num_classes"]
+                if num_classes == 1:
                     updated_mask = torch.sigmoid(output.clone()).squeeze(0)
                     predicted_mask = (updated_mask > self.config.get('threshold', 0.5)).int()
                 else:
