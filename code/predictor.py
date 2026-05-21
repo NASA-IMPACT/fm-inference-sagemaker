267a86572d0fc6aa891fc91dc620246c64558019
Leo Thomas <leo.thomas@mail.mcgill.ca>
2026-05-21 12:10:14 -0500

    Add geojson file upload to s3

--- code/predictor.py
+++ code/predictor.py
@@ -1,3 +1,6 @@
+from fileinput import filename
+from multiprocessing.dummy import connection
+
 import boto3
 import json
 import os
@@ -30,33 +33,34 @@ from skimage.morphology import disk, binary_closing
 
 from starlette.middleware.cors import CORSMiddleware
 
-
 app = FastAPI()
 
-CONFIG_FILENAME = os.environ.get('S3_CONFIG_FILENAME')
-CHECKPOINT_FILE = os.environ.get('CHECKPOINT_FILENAME')
-USECASE = os.environ.get('USECASE')
+CONFIG_FILENAME = os.environ.get("S3_CONFIG_FILENAME")
+CHECKPOINT_FILE = os.environ.get("CHECKPOINT_FILENAME")
+USECASE = os.environ.get("USECASE")
 
-def download_from_s3(s3_path, download_path='config'):
+
+def download_from_s3(s3_path, download_path="config"):
     session = assumed_role_session()
-    s3_connection = session.resource('s3')
+    s3_connection = session.resource("s3")
     bucket = s3_connection.Bucket(BUCKET_NAME)
-    filename = s3_path.split('/')[-1]
+    filename = s3_path.split("/")[-1]
     file_path = f"{download_path}/{filename}"
-    if not(os.path.exists(file_path)):
-        print('====================')
-        print(s3_path.replace(f's3://{BUCKET_NAME}/', ''), file_path)
+    if not (os.path.exists(file_path)):
+        print("====================")
+        print(s3_path.replace(f"s3://{BUCKET_NAME}/", ""), file_path)
         os.makedirs(download_path, exist_ok=True)
-        os.makedirs('predictions', exist_ok=True)
-        bucket.download_file(s3_path.replace(f's3://{BUCKET_NAME}/', ''), file_path)
+        os.makedirs("predictions", exist_ok=True)
+        bucket.download_file(s3_path.replace(f"s3://{BUCKET_NAME}/", ""), file_path)
     return file_path
 
 
 def load_model():
     config_file_path = download_from_s3(CONFIG_FILENAME)
-    model_weights_path = download_from_s3(CHECKPOINT_FILE, 'models')
+    model_weights_path = download_from_s3(CHECKPOINT_FILE, "models")
     infer = Infer(config_file_path, model_weights_path)
-    return { USECASE: infer }
+    return {USECASE: infer}
+
 
 MODELS = load_model()
 
@@ -65,6 +69,7 @@ def download_files(infer_date, layer, bounding_box):
     downloader = Downloader(infer_date, layer)
     return downloader.download_tiles(bounding_box)
 
+
 def save_cog(mosaic, profile, transform, filename):
     profile.update(
         {
@@ -72,13 +77,13 @@ def save_cog(mosaic, profile, transform, filename):
             "height": mosaic.shape[0],
             "width": mosaic.shape[1],
             "transform": transform,
-            "dtype": 'float32',
+            "dtype": "float32",
             "count": 1,
         }
     )
-    with rasterio.open(filename, 'w', **profile) as raster:
+    with rasterio.open(filename, "w", **profile) as raster:
         raster.write(mosaic, 1)
-    output_profile = cog_profiles.get('deflate')
+    output_profile = cog_profiles.get("deflate")
     output_profile.update(dict(BIGTIFF="IF_SAFER"))
     output_profile.update(profile)
 
@@ -97,7 +102,7 @@ def save_cog(mosaic, profile, transform, filename):
             quiet=True,
             in_memory=True,
         )
-        connection = boto3.client('s3')
+        connection = boto3.client("s3")
         connection.upload_fileobj(memory_file, BUCKET_NAME, filename)
 
     return f"s3://{BUCKET_NAME}/{filename}"
@@ -111,8 +116,8 @@ def post_process(detections, transform):
 
 
 def subset_geojson(geojson, bounding_box):
-    geom = [shape(i['geometry']) for i in geojson]
-    geom = gpd.GeoDataFrame({'geometry': geom})
+    geom = [shape(i["geometry"]) for i in geojson]
+    geom = gpd.GeoDataFrame({"geometry": geom})
     bbox = {
         "type": "Polygon",
         "coordinates": [
@@ -126,8 +131,8 @@ def subset_geojson(geojson, bounding_box):
         ],
     }
     bbox = shape(bbox)
-    bbox = gpd.GeoDataFrame({'geometry': [bbox]})
-    return json.loads(geom.overlay(bbox, how='intersection').to_json())
+    bbox = gpd.GeoDataFrame({"geometry": [bbox]})
+    return json.loads(geom.overlay(bbox, how="intersection").to_json())
 
 
 def batch(tiles, spacing=60):
@@ -138,16 +143,16 @@ def batch(tiles, spacing=60):
 
 def infer(model_id, infer_date, bounding_box, terramind=False, file_links=[]):
     if model_id not in MODELS:
-        response = {'statusCode': 422}
+        response = {"statusCode": 422}
         return JSONResponse(content=jsonable_encoder(response))
     inference = MODELS[model_id]
     all_tiles = list()
     geojson_list = list()
-    geojson = {'type': 'FeatureCollection', 'features': []}
+    geojson = {"type": "FeatureCollection", "features": []}
     print(model_id, infer_date, bounding_box, terramind, file_links)
     if terramind:
         for file_link in file_links:
-            all_tiles.append(download_from_s3(file_link, './data'))
+            all_tiles.append(download_from_s3(file_link, "./data"))
     else:
         for layer in LAYERS:
             tiles = download_files(infer_date, layer, bounding_box)
@@ -156,7 +161,7 @@ def infer(model_id, infer_date, bounding_box, terramind=False, file_links=[]):
 
     start_time = time.time()
     mosaic = []
-    s3_link = ''
+    s3_link = ""
     if all_tiles:
         try:
             torch.cuda.synchronize()
@@ -168,15 +173,12 @@ def infer(model_id, infer_date, bounding_box, terramind=False, file_links=[]):
                     batch_results, batch_profiles = inference.infer(tiles, terramind)
                     results.extend(batch_results)
                     profiles.extend(batch_profiles)
-                    print('Done inference')
+                    print("Done inference")
             memory_files = list()
             torch.cuda.empty_cache()
             for index, profile in enumerate(profiles):
                 memfile = MemoryFile()
-                profile.update({
-                    'count': 1,
-                    'dtype': 'float32'
-                })
+                profile.update({"count": 1, "dtype": "float32"})
                 with memfile.open(**profile) as memoryfile:
                     memoryfile.write(results[index][0], 1)
                     print(index, results[index].min(), results[index].max())
@@ -187,41 +189,57 @@ def infer(model_id, infer_date, bounding_box, terramind=False, file_links=[]):
             [memfile.close() for memfile in memory_files]
             prediction_filename = f"predictions/{start_time}-predictions.tif"
 
-            s3_link = save_cog(mosaic[0], profile, transform, prediction_filename)
+            cog_s3_link = save_cog(mosaic[0], profile, transform, prediction_filename)
 
             geojson = post_process(mosaic[0], transform)
+            geojson_filename = f"predictions/{start_time}-predictions.geojson"
+
+            with MemoryFile().open() as memoryfile:
+                json.dump(geojson, memoryfile)
+                connection = boto3.client("s3")
+                connection.upload_fileobj(memoryfile, BUCKET_NAME, geojson_filename)
+
+            geojson_s3_link = f"s3://{BUCKET_NAME}/{geojson_filename}"
 
             for geometry in geojson:
                 updated_geometry = PostProcess.convert_geojson(geometry)
                 geojson_list.append(updated_geometry)
             geojson = subset_geojson(geojson_list, bounding_box)
         except Exception as e:
-            print('!!! infer error', infer_date, model_id, bounding_box, e)
+            print("!!! infer error", infer_date, model_id, bounding_box, e)
             torch.cuda.empty_cache()
         print("!!! Infer Time:", time.time() - start_time)
     del inference
     gc.collect()
 
     return {
-        model_id: {'s3_link': s3_link, 'predictions': geojson}
+        model_id: {
+            "cog_s3_link": cog_s3_link,
+            "geojson_s3_link": geojson_s3_link,
+            "predictions": geojson,
+        }
     }
 
-@app.post('/invocations')
+
+@app.post("/invocations")
 async def infer_from_model(request: Request):
     instances = await request.json()
-    if instances.get('generation'):
-        inferance = InferGeneration(instances['input_file'])
-        pred = inferance.tiled_infer(reduce=instances.get('reduce', True))
+    if instances.get("generation"):
+        inferance = InferGeneration(instances["input_file"])
+        pred = inferance.tiled_infer(reduce=instances.get("reduce", True))
         return JSONResponse(content=jsonable_encoder(pred))
     model_id = USECASE
-    infer_date = instances.get('date')
-    bounding_box = instances.get('bounding_box')
-    terramind = instances.get('terramind', False)
-    file_links = instances.get('file_urls', [])
+    infer_date = instances.get("date")
+    bounding_box = instances.get("bounding_box")
+    terramind = instances.get("terramind", False)
+    file_links = instances.get("file_urls", [])
     print(instances)
-    final_geojson = infer(model_id, infer_date, bounding_box, terramind=terramind, file_links=file_links)
+    final_geojson = infer(
+        model_id, infer_date, bounding_box, terramind=terramind, file_links=file_links
+    )
     return JSONResponse(content=jsonable_encoder(final_geojson))
 
-@app.get('/ping')
+
+@app.get("/ping")
 async def ping(request: Request):
-    return { 'successCode': 200, 'message': 'pong'}
+    return {"successCode": 200, "message": "pong"}
